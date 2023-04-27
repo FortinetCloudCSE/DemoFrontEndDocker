a33f62c68ce8b0ea508df8c71250c2b0519c3680
Robert Reris <rreris@fortinet.com>
2023-04-27 14:01:15 -0400

    generate-site.sh udpate

--- scripts/generate-site.sh
+++ scripts/generate-site.sh
@@ -4,7 +4,11 @@
 # where $TOP_LEVEL is the top level of the repo directory.
 
 TOP_LEVEL=$(git rev-parse --show-toplevel)
+
+[[ "$#" > 1 ]] && echo "Usage: ./generate-site.sh [output path]" && exit 0
+[[ "$#" < 1 ]] && OUT_PATH=$TOP_LEVEL || OUT_PATH=$1
+
 docker build -t app-image:latest -f $TOP_LEVEL/DockerfileSite $TOP_LEVEL
 DOCK_CONT=$(docker run -d app-image:latest)
-docker cp $DOCK_CONT:/home/app/public $TOP_LEVEL/static-site-files
+docker cp $DOCK_CONT:/home/app/public $OUT_PATH/static-site-files
 docker rm -f $DOCK_CONT
